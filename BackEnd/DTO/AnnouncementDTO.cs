bd07444e0d99f04b8be3b982e2d1ddacbff7b2e8
Hryhoriy K. <101716956+geloft@users.noreply.github.com>
2024-04-28 17:26:09 +0300

    Update AnnouncementDTO.cs

--- BackEnd/DTO/AnnouncementDTO.cs
+++ BackEnd/DTO/AnnouncementDTO.cs
@@ -1,16 +1,35 @@
 ﻿using BackApi.DataTypes;
+using Newtonsoft.Json;
 
 namespace BackEnd.DTO
 {
     public class AnnouncementDTO
     {
+        [JsonProperty("id")]
         public int Id { get; set; }
+
+        [JsonProperty("consumer_id")]
         public int ConsumerId { get; set; }
+
+        [JsonProperty("category_id")]
         public int CategoryId { get; set; }
+
+        [JsonProperty("title")]
         public string Title { get; set; }
+
+        [JsonProperty("subtitle")]
         public string Subtitle { get; set; }
+
+        [JsonProperty("description")]
         public string Description { get; set; }
+
+        [JsonProperty("tags")]
         public List<string> Tags { get; set; }
+
+        [JsonProperty("status")]
         public AnnouncementStatus Status { get; set; }
+
+        [JsonProperty("created_date")]
+        public DateTime CreatedDate { get; set; }
     }
 }
